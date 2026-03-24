d555a4cdd811f028165d62ab517f733e350fc397
Sylvester <25aima46@kristujayanti.com>
2026-03-25 00:04:02 +0530

    final port fix

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,8 @@ app.get("/orders", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Server running 🚀");
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
