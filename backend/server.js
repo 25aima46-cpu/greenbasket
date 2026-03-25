cedd5de794e0e7b59175c9fc387822a32bf7f491
Sylvester <25aima46@kristujayanti.com>
2026-03-25 07:49:15 +0530

    fixed syntax error

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ db.connect((err) => {
 
 // Home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Add order
